779c0355662ac0282ad01c745ab16c2d3d19c9ec
Ronnie Nigash <nigash@uw.edu>
2015-07-18 14:23:47 -0700

    Update method parameters and iterate through ObjectStyle list

--- SigFormatting.jsx
+++ SigFormatting.jsx
@@ -1,13 +1,19 @@
 Main();
 function Main() {
 	if (app.documents.length > 0) {
-		MoveChangeObjectSize(@param1, @param2);
+		MoveChangeObjectSize(["Object Style 1", "Object Style 2"], [5, 6]);
 	} else {
 		alert("No open documents.");
 	}
 }
 
-function MoveChangeObjectSize(param1, param2) {
+function MoveChangeObjectSize(objectStyleName, changeWidth) {
 	var doc = app.activeDocument;
+	for (i = 0; i < objectStyleName.length; i++) {
+		doc.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.PICAS;
+		doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.PICAS;
+		
+		// TODO: fill out logic of finding objects, moving and changing sizes
+	}
 
 }
