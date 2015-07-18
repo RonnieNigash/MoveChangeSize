9fe9bb8860253ada92e791040dac37410765e4d7
Ronnie Nigash <nigash@uw.edu>
2015-07-18 14:38:17 -0700

    Update documentation

--- SigFormatting.jsx
+++ SigFormatting.jsx
@@ -1,6 +1,7 @@
 Main();
 function Main() {
 	if (app.documents.length > 0) {
+		// Names of Object Styles and their corresponding changes in width
 		MoveChangeObjectSize(["Object Style 1", "Object Style 2"], [5, 6]);
 	} else {
 		alert("No open documents.");
@@ -13,16 +14,19 @@ function MoveChangeObjectSize(objectStyleName, changeWidth) {
 		doc.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.PICAS;
 		doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.PICAS;
 		
-		// TODO: fill out logic of finding objects, moving and changing widths
 		app.findObjectPreference.appliedObjectsStyles = objectStyleName[i];
 		var foundObjects = doc.findObject();
 
-		// TODO: Look through found objects and change position/width
 		for (j = 0; j < foundObjects.length; j++) {
 			frame = foundObjects[i];
-			frame.move([X_Index, Y_Index]);
+			frame.move([0, 11.775]); // Moves the object to the point (0, 11.775) in picas
 			gb = frame.geometricBounds;
 
+			// gb[0] = y1; gb[1] = x1; gb[2] = y2; gb[3] = x2;
+			// We keep the y1, x1, y2 the same.
+			// Change width by adding to gb[3] -> (gb[1] + DesiredWidth)
+			// Change height by adding to gb[0] -> (gb[0] + DesiredHeight) --> here we have no change
+			//		 var changeHeight = 0; -> var height = gb[2] - gb[0];
 			frame.geometricBounds = [gb[0], gb[1], gb[2], gb[1]+changeWidth[i]];
 		}
 	}
