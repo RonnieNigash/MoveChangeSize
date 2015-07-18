3daa71a6d3cf82f8500ec36b9212d7807fe73eb6
Ronnie Nigash <nigash@uw.edu>
2015-07-18 14:20:03 -0700

    Structure functions

--- SigFormatting.jsx
+++ SigFormatting.jsx
@@ -1,8 +1,13 @@
 Main();
 function Main() {
-
-
-
+	if (app.documents.length > 0) {
+		MoveChangeObjectSize(@param1, @param2);
+	} else {
+		alert("No open documents.");
+	}
 }
 
+function MoveChangeObjectSize(param1, param2) {
+	var doc = app.activeDocument;
 
+}
